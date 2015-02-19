2ae5402fe30ac3917b266e333eb7c86b8cb9fec2
ivanpepelko <ivan.pepelko@gmail.com>
2015-02-19 14:13:41 +0100

    try2

---
+++ PepelkoDraw/GraphicalObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Xml;
+
+namespace PepelkoDraw {
+    abstract class GraphicalObject {
+
+        protected int StartX {
+            get;
+            set;
+        }
+
+        protected int StartY {
+            get;
+            set;
+        }
+
+        protected int EndX {
+            get;
+            set;
+        }
+
+        protected int EndY {
+            get;
+            set;
+        }
+
+        protected Pen p;
+
+        public GraphicalObject(int sx, int sy, int ex, int ey, Color c, int penw) {
+            StartX = sx;
+            StartY = sy;
+            EndX = ex;
+            EndY = ey;
+            p = new Pen(c, penw);
+        }
+
+        public abstract void Draw(Graphics g);
+
+        public XmlElement GetExportElement() {
+            XmlDocument doc = new XmlDocument();
+
+            string type = this.GetType().ToString();
+
+            XmlElement obj = doc.CreateElement(type);
+
+            obj.SetAttribute("startx", StartX.ToString());
+            obj.SetAttribute("starty", StartY.ToString());
+            obj.SetAttribute("endx", EndX.ToString());
+            obj.SetAttribute("endy", EndY.ToString());
+            obj.SetAttribute("color-a", p.Color.A.ToString());
+            obj.SetAttribute("color-r", p.Color.R.ToString());
+            obj.SetAttribute("color-g", p.Color.G.ToString());
+            obj.SetAttribute("color-b", p.Color.B.ToString());
+            obj.SetAttribute("width", p.Width.ToString());
+
+            return obj;
+        }
+
+    }
+}
---
+++ PepelkoDraw/MainWindow_Drawing.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PepelkoDraw {
+    public partial class MainWindow : Form {
+
+        private void MainWindow_Paint(object sender, PaintEventArgs e) {
+            MasterGraphics = e.Graphics;
+            MasterGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            
+            if (IsMouseDown) {
+                CurrentObject = CreateGraphicalObject(SelectedTool, StartX, StartY, EndX, EndY);
+                CurrentObject.Draw(MasterGraphics);
+            }
+
+            foreach (GraphicalObject go in Objects) {
+                go.Draw(MasterGraphics);
+            }
+
+            if (Objects.Count == 0) {
+                undoActionButton.Enabled = false;
+            } else {
+                undoActionButton.Enabled = true;
+            }
+
+            if (RedoBuffer.Count == 0) {
+                redoActionButton.Enabled = false;
+                RedoEnabled = false;
+            } else {
+                redoActionButton.Enabled = true;
+            }
+
+        }
+
+        private void colorButton_Click(object sender, EventArgs e) {
+            colorPicker.ShowDialog();
+            colorButton.BackColor = colorPicker.Color;
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseEventArgs e) {
+            Cursor = Cursors.Cross;
+            IsMouseDown = true;
+            StartX = e.X;
+            StartY = e.Y;
+        }
+
+        private void MainWindow_MouseUp(object sender, MouseEventArgs e) {
+            Cursor = Cursors.Default;
+            IsMouseDown = false;
+
+            Objects.Add(CreateGraphicalObject(SelectedTool, StartX, StartY, e.X, e.Y));
+
+            RedoEnabled = false;
+            RedoBuffer.Clear();
+            Refresh();
+        }
+
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e) {
+            EndX = e.X;
+            EndY = e.Y;
+            Refresh();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (undoActionButton.Enabled) {
+                RedoBuffer.Add(Objects.Last());
+                Objects.Remove(Objects.Last());
+                RedoEnabled = true;
+                Refresh();
+            }
+        }
+
+        private void redoActionButton_Click(object sender, EventArgs e) {
+            if (redoActionButton.Enabled && RedoEnabled) {
+                Objects.Add(RedoBuffer.Last());
+                RedoBuffer.Remove(RedoBuffer.Last());
+                Refresh();
+            }
+        }
+
+    }
+}
